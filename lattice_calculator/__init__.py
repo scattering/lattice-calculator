08271a4435de34ac102339e55ad2a3bd8f302506
williamratcliff <william.ratcliff@gmail.com>
2026-01-04 05:57:29 -0500

    Export modvec, scalar, and other vector functions
    - Rename lattice-calculator.py to lattice_calculator.py for proper Python imports
    - Export modvec, scalar, angle, angle2, reciprocate, vector from __init__.py
    - These functions are needed by rescalculator for resolution calculations

--- lattice_calculator/__init__.py
+++ lattice_calculator/__init__.py
@@ -1,5 +1,7 @@
 from .lattice_calculator import (
-    Lattice, Orientation, Instrument, 
+    Lattice, Orientation, Instrument,
     py_calculate_hkl_e_from_angles,
-    SpecWhere, SpecGoTo
+    SpecWhere, SpecGoTo,
+    modvec, scalar, angle, angle2, reciprocate, vector,
+    StandardSystem, S2R, R2S
 )
